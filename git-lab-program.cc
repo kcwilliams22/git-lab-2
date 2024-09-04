8327cd7b8ad0e70cc1443acd3f0cf8f5bc1d3cbd
kcwilliams22 <kw723423@ohio.edu>
2024-09-04 18:47:45 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Katrin Williams
+ *        Date: 9/4/2024
+ * Description: Lab 2, Github code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout<<"Hello Git!!"<< endl;
+    return 0;
+}// main
